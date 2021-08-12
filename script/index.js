0b0179c309ed4982ce193d945500ece2c208815e
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2021-08-12 16:32:49 +0200

    randomFood

--- script/index.js
+++ script/index.js
@@ -14,12 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
   //#endregion
 
   //#region Zmienne
-  const stop = document.querySelector(".stop");
-  const width = 15;
-  let snake = [0, 1, 2];
-  let direction = 1;
+  const stop = document.querySelector(".stop")
+  const width = 15
+  let snake = [0, 1, 2]
+  let direction = 1
+  let foodIndex = 0
   //#endregion
-
   //#region rysujemy węża
   const drawSnake = () => {
     snake.forEach((item) => {
@@ -28,22 +28,39 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   drawSnake();
   //#endregion
+  
   //#region usuwamy węża
   const removeSnake = () => {
     field.forEach((item) => item.classList.remove("snake"));
   };
   //#endregion
+
   //#region ruch węża
   const moveSnake = () => {
     removeSnake();
     for (let i = 0; i < snake.length; i++) {
       snake[i] += direction;
     }
-    drawSnake();
+    drawSnake()
+    eatFood()
   };
   let moveSnakeId = setInterval(moveSnake, 1000);
   stop.addEventListener("click", () => clearInterval(moveSnakeId));
   //#endregion
+ 
+  //#region natrafienie na jedzoanko
+  function eatFood (){
+    field.forEach( item => {
+      if(item.classList.contains("food") && item.classList.contains("snake")){
+        item.classList.remove("food")
+        let partSnake = snake[snake.length - 1] + 1
+        snake.push(partSnake)
+        randomFood()
+      }
+    })
+  }
+  //#endregion
+
   //#region kirunek węża
   function directionSnake(e) {
     switch (e.keyCode) {
@@ -62,5 +79,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   document.addEventListener("keyup", directionSnake);
+  //#endregion
+  
+  //#region lusujemy pozycję jedzonka
+  function randomFood(){
+    do{
+      foodIndex = Math.floor(Math.random() * field.length)
+    } while (field[foodIndex].classList.contains("snake"))
+    field[foodIndex].classList.add("food")
+  }
+  randomFood()
   //#endregion
 });
